ff0f1f79bd3aeae89106d1114ba60ca46483652f
Ridoan Saleh Nasution <ridoansalehnasution98@gmail.com>
2018-05-12 22:22:33 +0700

    Dev (#1)
    * remove bootstrap etc
    
    * upgrage babel-preset-es2015
    
    * remove bundle file
    
    * remove bootstrap classes etc

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         exclude: '/node_modules',
         loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015"]
+          presets: ["react", "env"]
         }
       },
       {
